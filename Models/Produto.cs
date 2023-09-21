c24ee73fbb19cf10d329a186dabe050b50973f69
Gabbyroba <gabbyramosbr2@gmail.com>
2023-09-21 14:29:06 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Produto.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebCRUDMVCSQL.Models
+{
+    [Table("ControledeProdutos")]
+
+
+    public class Produto
+    {
+        [Column("Id")]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+
+
+        [Column("Quantidade")]
+        [Display(Name = "Quantidade")]
+        public string Quantidade { get; set; }
+
+
+        [Column("Preço")]
+        [Display(Name = "Preço")]
+        public string Preço { get; set; }
+    }
+}
